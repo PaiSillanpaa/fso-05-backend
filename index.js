f0d29b0dce54299cac71bebc88d08a94ac3cbc0e
PaiSillanpaa <sillanpaapai@gmail.com>
2025-04-03 12:42:30 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
 
 app.use(express.json());
 
@@ -46,12 +49,41 @@ app.delete("/api/notes/:id", (request, response) => {
   response.status(204).end();
 });
 
+// app.post("/api/notes", (request, response) => {
+//   const maxId =
+//     notes.lenght > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
+
+//   const note = (request.bodynote.id = String(maxId + 1));
+
+//   notes = notes.concat(note);
+
+//   response.json(note);
+// });
+
+const generateId = () => {
+  const maxId =
+    notes.lenght > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
+  return String(maxId + 1);
+};
+
 app.post("/api/notes", (request, response) => {
-  const note = request.body;
-  console.log(note);
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
   response.json(note);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
